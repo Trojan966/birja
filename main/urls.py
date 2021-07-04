e12d41bd4a7c5a39e50a4b7b79fd29b47905931f
python339 <frederikk123@gmail.com>
2021-07-05 00:43:03 +0300

    определение кода в ссылке

--- main/urls.py
+++ main/urls.py
@@ -1,10 +1,13 @@
 from django.contrib import admin
-from django.urls import path,include
+from django.urls import path, include
 from . import views
+from .views import main_view
 
 urlpatterns = [
     path('',views.index),
     path('en/',views.indexen),
     path('client/',views.client),
-    path('admin/',views.admin)
+    path('admin/',views.admin),
+    path('ref', main_view, name='main-view'),
+    path('ref/<str:ref_code>', main_view, name='main-view'),
 ]
--- main/views.py
+++ main/views.py
@@ -1,11 +1,29 @@
 from django.shortcuts import render
+from profiles.models import Profile
+
+
+def main_view(request, *args, **kwargs):
+    code = str(kwargs.get('ref_code'))
+    try:
+        profile = Profile.objects.get(code=code)
+        request.session['ref_profile'] = profile.id
+    except:
+        pass
+    return render(request, 'main.html', {})
+
 
 # Create your views here.
 def index(request):
-    return render(request,'main/index.html')
+    return render(request, 'main/index.html')
+
+
 def indexen(request):
-    return render(request,'main/inen.html')
+    return render(request, 'main/inen.html')
+
+
 def client(request):
-    return render(request,'main/client panel.html')
+    return render(request, 'main/client panel.html')
+
+
 def admin(request):
-    return render(request,'main/panel.html')
+    return render(request, 'main/panel.html')
