a6c0289bb7903a70110bcfb9b41220af3733c84e
Trojan <megatawer@list.ru>
2021-07-09 14:44:32 +0300

    сделал фронт кдтень ажминки фулл

--- main/models.py
+++ main/models.py
@@ -1,6 +1,11 @@
 from django.db import models
 from django.contrib.auth.models import User
 # Create your mo
+class Balance(models.Model):
+    btc = models.CharField(max_length=20)
+    eth = models.CharField(max_length=20)
+    bch = models.CharField(max_length=20)
+    lth = models.CharField(max_length=20)
 class Transaction(models.Model):
     user = models.CharField(max_length=40)
     amount = models.FloatField()
