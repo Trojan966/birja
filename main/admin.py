1406f9b77c645e3840ef65537c1a2b94cee81e67
Trojan <megatawer@list.ru>
2021-07-09 21:25:48 +0300

    сделал баланс и модели

--- main/admin.py
+++ main/admin.py
@@ -1,5 +1,9 @@
 from django.contrib import admin
 from .models import *
 # Register your models here.
-
-admin.site.register(Transaction)
+admin.site.register(Balance)
+admin.site.register(TransactionUtc)
+admin.site.register(TransactionBch)
+admin.site.register(TransactionBnb)
+admin.site.register(TransactionBtc)
+admin.site.register(TransactionEth)
--- main/models.py
+++ main/models.py
@@ -4,15 +4,45 @@ from django.contrib.auth.models import User
 class Balance(models.Model):
     btc = models.CharField(max_length=20)
     eth = models.CharField(max_length=20)
+    bnb = models.CharField(max_length=20)
     bch = models.CharField(max_length=20)
-    lth = models.CharField(max_length=20)
-class Transaction(models.Model):
+    utc = models.CharField(max_length=20)
+class TransactionBtc(models.Model):
     user = models.CharField(max_length=40)
     amount = models.FloatField()
     currency = models.CharField(max_length=5)
     method = models.CharField(max_length=60)
     status = models.CharField(max_length=60)
     date = models.DateTimeField()
+class TransactionBnb(models.Model):
+    user = models.CharField(max_length=40)
+    amount = models.FloatField()
+    currency = models.CharField(max_length=5)
+    method = models.CharField(max_length=60)
+    status = models.CharField(max_length=60)
+    date = models.DateTimeField()
+class TransactionEth(models.Model):
+    user = models.CharField(max_length=40)
+    amount = models.FloatField()
+    currency = models.CharField(max_length=5)
+    method = models.CharField(max_length=60)
+    status = models.CharField(max_length=60)
+    date = models.DateTimeField()
+class TransactionBch(models.Model):
+    user = models.CharField(max_length=40)
+    amount = models.FloatField()
+    currency = models.CharField(max_length=5)
+    method = models.CharField(max_length=60)
+    status = models.CharField(max_length=60)
+    date = models.DateTimeField()
+class TransactionUtc(models.Model):
+    user = models.CharField(max_length=40)
+    amount = models.FloatField()
+    currency = models.CharField(max_length=5)
+    method = models.CharField(max_length=60)
+    status = models.CharField(max_length=60)
+    date = models.DateTimeField()
+
 
     def __str__(self):
         return self.user.username
